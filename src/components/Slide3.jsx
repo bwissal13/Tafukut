e0e094bbf5d7e4a40094d239b893177ba8eed1a8
bwissal <baazizwissal13@gmail.com>
2024-06-29 12:14:22 +0100

    update

--- src/components/Slide3.jsx
+++ src/components/Slide3.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import '../assets/styles/Slide1.css';
 import '../assets/styles/Slide2.css';
 import '../assets/styles/Sign.css';
+import { Link } from 'react-router-dom';
 export default function Slide3() {
     return (
         <>
@@ -11,7 +12,7 @@ export default function Slide3() {
                     We envision a world where coding education is accessible, interactive, and affordable for everyone. Our goal is to empower individuals to learn and grow in the field of technology. We provide high-quality resources to bridge the gap between education and opportunity. We believe everyone should have the chance to excel in the tech industry.</p>
                 <h2 className="slide2_title-Features">Let’s build from here</h2>
                 <p className="slide3_paragraph">The world’s leading platform for interactive coding tutorials.</p>
-                <button className="sign__button slide3__button">Start learning now</button>
+                <Link to="/Tutorials" className="sign__button slide3__button"  >Start learning now</Link>
             </div>
 
         </>)
